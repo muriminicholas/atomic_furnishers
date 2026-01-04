9697587c5a71757413c6941a6f340e5a8b3cea63
muriminicholas <muriminicholas197@gmail.com>
2026-01-04 13:12:45 -0500

    tailwindcss fixing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,32 @@ const Footer = () => {
     >
       <p className="text-sm">© 2026 Atomic Furnishers and Interiors Nairobi — All rights reserved.</p>
       <div className="flex justify-center space-x-4 mt-4">
-        <a href="#" className="hover:text-teak">Instagram</a>
-        <a href="#" className="hover:text-teak">TikTok</a>
-        <a href="#" className="hover:text-teak">Pinterest</a>
-      </div>
+  <a 
+    href="www.instagram.com[your-username]" 
+    target="_blank" 
+    rel="noopener noreferrer" 
+    className="hover:text-teak"
+  >
+    Instagram
+  </a>
+  <a 
+    href="www.tiktok.com@[your-username]" 
+    target="_blank" 
+    rel="noopener noreferrer" 
+    className="hover:text-teak"
+  >
+    TikTok
+  </a>
+  <a 
+    href="https://www.pinterest.com/[your-username]" 
+    target="_blank" 
+    rel="noopener noreferrer" 
+    className="hover:text-teak"
+  >
+    Pinterest
+  </a>
+</div>
+
     </motion.footer>
   );
 };
